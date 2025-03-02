17881567c12b08e88e1c0f4015092b00e55f187a
Polina <yenina2000@inbox.ru>
2025-03-02 17:39:17 +0300

    LocalDateTimeSerializer

---
+++ src/main/java/org/example/system/serializers/LocalDateTimeSerializer.java
@@ -0,0 +1,23 @@
+package org.example.system.serializers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer implements JsonSerializer<LocalDate> {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public JsonElement serialize(final LocalDate date, final Type typeOfSrc,
+                                 final JsonSerializationContext context) {
+        return new JsonPrimitive(date.format(formatter));
+    }
+
+}
