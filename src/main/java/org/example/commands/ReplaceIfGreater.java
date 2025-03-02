8a07c4a00363d7cd536bba1ce3dfe69be62c5fff
b00bilda <anzhel1ka.yakub1nskaya@gmail.com>
2025-03-02 16:31:37 +0300

    replace_if_greater command

---
+++ src/main/java/org/example/commands/ReplaceIfGreater.java
@@ -0,0 +1,17 @@
+package org.example.commands;
+
+public class ReplaceIfGreater extends Command{
+    public ReplaceIfGreater(){
+        super("replace_if_greater");
+    }
+
+    @Override
+    public void execute(String[] args) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+}
