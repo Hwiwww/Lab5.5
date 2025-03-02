732e8f8da6ee1d4de7f3c2d94df4fcad076a9caa
b00bilda <anzhel1ka.yakub1nskaya@gmail.com>
2025-03-02 17:25:05 +0300

    json deserializer for local date

---
+++ src/main/java/org/example/system/deserializers/LocalDateDeserializer.java
@@ -0,0 +1,18 @@
+package org.example.system.deserializers;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public LocalDate deserialize(final JsonElement json, final Type typeOfT,
+                                 final JsonDeserializationContext context) throws JsonParseException {
+        return LocalDate.parse(json.getAsString(), formatter);
+    }
+}
