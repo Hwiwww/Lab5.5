f0d1afc181450e32eaf934724a1bfd524f4b36c6
Polina <yenina2000@inbox.ru>
2025-03-02 16:46:46 +0300

    Coordinates

---
+++ src/main/java/org/example/data/Coordinates.java
@@ -0,0 +1,35 @@
+package org.example.data;
+
+public class Coordinates {
+    private double x; //Максимальное значение поля: 909
+    private long y;
+
+    public Coordinates(double x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinates() {
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        if (x < 909) {
+            this.x = x;
+        } else {
+            throw new IllegalArgumentException("x cannot be greater than 909.");
+        }
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+}
+
