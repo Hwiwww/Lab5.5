6c1cce0848a5cce1d63522115d7f6abebf639413
Polina <yenina2000@inbox.ru>
2025-03-02 16:13:35 +0300

    FilterLessThanWeight

---
+++ src/main/java/org/example/commands/FilterLessThanWeight.java
@@ -0,0 +1,28 @@
+package org.example.commands;
+
+import org.example.system.CollectionManager;
+
+public class FilterLessThanWeight extends Command {
+    public FilterLessThanWeight() {
+        super("filter_less_than_weight");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        try {
+            CollectionManager.getCollection().forEach((key, dragon) -> {
+                int comparisonWeight = Integer.parseInt(args[0]);
+                if (dragon.getWeight() < comparisonWeight) {
+                    System.out.println(dragon.toString());
+                }
+            });
+        } catch (IllegalArgumentException e) {
+            System.out.println("Something wrong with arguments");
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "outputs elements which value of weight is lower than given";
+    }
+}
